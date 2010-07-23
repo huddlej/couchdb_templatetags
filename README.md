6ef3f97785e95433a54af8ea26297e8df90909e6
John Huddleston <huddlej@gmail.com>
2010-07-23 09:48:29 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# CouchDB template tags for Django
+
+[CouchDB](http://couchdb.apache.org) template tag for
+[Django](http://www.djangoproject.com) includes content from any CouchDB URL
+including lists, shows, and views.
+
+## Configuration
+
+Specify your primary CouchDB server with the ``COUCHDB_SERVER`` variable in your
+Django settings.py. If ``COUCHDB_SERVER`` isn't set, the template tag will
+default to ``http://localhost:5984``.
+
+## Usage
+
+The first argument is a path relative to your CouchDB server. For example, the
+following example outputs ``{"couchdb":"Welcome","version":"0.10.0"}``.
+
+    {% couchdb / %}
+
+Optional query parameters are specified using keyword arguments. For example,
+the following example includes the output of a CouchDB list with documents
+included:
+
+    {% couchdb /mydb/_design/mydesigndoc/_list/mylist/myview include_docs="true" %}
+
+Query parameter values must be valid [JSON](http://www.json.org/). Use the
+``jsonify`` filter on any keyword arguments that need to be converted to JSON
+before being passed to the database:
+
+    {% couchdb /mydb/_design/mydesigndoc/_list/mylist/myview key=string_with_spaces|jsonify %}
---
+++ templatetags/couchdb.py
@@ -0,0 +1,99 @@
+"""
+CouchDB template tag based on Django's built-in "url" template tag.
+"""
+from django import template
+from django.conf import settings
+from django.template import Node, TemplateSyntaxError
+from django.utils import simplejson
+from django.utils.encoding import smart_str
+from django.utils.safestring import mark_safe
+import re
+import urllib
+
+
+register = template.Library()
+# Regex for token keyword arguments
+kwarg_re = re.compile(r"(?:(\w+)=)?(.+)")
+
+
+@register.filter
+def jsonify(o):
+    """
+    Based on Django snippet 1250:
+    http://djangosnippets.org/snippets/1250/
+    """
+    return mark_safe(simplejson.dumps(o))
+
+
+def get_content(url, **kwargs):
+    if hasattr(settings, "COUCHDB_SERVER"):
+        server = settings.COUCHDB_SERVER
+    else:
+        server = "http://localhost:5984"
+
+    url = urllib.basejoin(server, url)
+    if kwargs:
+        url = "%s?%s" % (url, urllib.urlencode(kwargs))
+
+    return urllib.urlopen(url).read().strip()
+
+
+class CouchDbNode(Node):
+    def __init__(self, url, kwargs, asvar):
+        self.url = url
+        self.kwargs = kwargs
+        self.asvar = asvar
+
+    def render(self, context):
+        kwargs = dict([(smart_str(k,'ascii'), v.resolve(context))
+                       for k, v in self.kwargs.items()])
+        content = get_content(self.url, **kwargs)
+        if self.asvar:
+            context[self.asvar] = content
+            return ''
+        else:
+            return content
+
+
+def couchdb(parser, token):
+    """
+    Returns the content associated with a CouchDB URL and optional parameters.
+
+    For example, use the following code to embed the content of a CouchDB list
+    with an include_docs parameter::
+
+        {% couchdb /mydb/_design/mydesigndoc/_list/mylist/myview include_docs="true" %}
+
+    These arguments will be used to get content from the following URL::
+
+        http://localhost:5984/mydb/_design/mydesigndoc/_list/mylist/myview?include_docs=true
+
+    The first argument is a CouchDB url relative to the server defined as
+    ``COUCHDB_SERVER`` in settings.py. Keyword arguments are space-separated
+    values that will be used to build a query string for the URL.
+    """
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise TemplateSyntaxError("'%s' takes at least one argument"
+                                  " (url relative to CouchDB server)" % bits[0])
+    url = bits[1]
+    kwargs = {}
+    asvar = None
+    bits = bits[2:]
+    if len(bits) >= 2 and bits[-2] == 'as':
+        asvar = bits[-1]
+        bits = bits[:-2]
+
+    # Now all the bits are parsed into new format,
+    # process them as template vars
+    if len(bits):
+        for bit in bits:
+            match = kwarg_re.match(bit)
+            if not match:
+                raise TemplateSyntaxError("Malformed arguments to couchdb tag")
+            name, value = match.groups()
+            if name:
+                kwargs[name] = parser.compile_filter(value)
+
+    return CouchDbNode(url, kwargs, asvar)
+couchdb = register.tag(couchdb)
